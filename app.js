a8e50a05858f71f6153b9ee6c6261368c285317d
Natia Khomasuridze <natiakhoma@gmail.com>
2019-01-17 16:27:48 -0500

    second draft

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
 
-const models = require('./models');
+const wikiRouter = require ('./routes/wiki') //folder name
+const userRouter = require ('./routes/user') //folder name
+
+const models = require('./models'); //folder
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.static('styleSheets')) //express have an access to styleSheet folder
+app.use('/wiki', wikiRouter);
+app.use('/user', userRouter);
 
 const PORT = 3000;
 
@@ -22,6 +27,7 @@ const init = async () => {
 
 
 init();
+
 
 
 // db.authenticate()
